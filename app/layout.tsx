7cb7a953822b5d5f16d61520ad9b3b40ee27dffd
retailrocketbackup <henriquez.alan@gmail.com>
2025-09-22 23:08:48 -0600

    add meta pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,11 +3,12 @@ import type { Metadata } from "next"
 import { RocknRoll_One } from "next/font/google"
 import { Suspense } from "react"
 import { Analytics } from "@vercel/analytics/react"
+import Script from 'next/script'
 import "./globals.css"
 
 const rocknrollOne = RocknRoll_One({
   subsets: ["latin"],
-  weight: "400", // RocknRoll One only has one weight
+  weight: "400",
 })
 
 export const metadata: Metadata = {
@@ -23,6 +24,29 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es">
+      <head>
+        {/* -- 2. Pega el código base del Píxel de Meta aquí -- */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '30721758817438946');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img height="1" width="1" style={{display: 'none'}}
+            src="https://www.facebook.com/tr?id=30721758817438946&ev=PageView&noscript=1"
+          />
+        </noscript>
+        {/* -- Fin del código del Píxel -- */}
+      </head>
       <body className={`font-sans ${rocknrollOne.className}`}>
         <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>
         <Analytics />
